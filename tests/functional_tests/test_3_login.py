79e502a1191fe26b1459de4384490447ee5f8c53
LekishviliRati <lekishvili.rati@gmail.com>
2021-08-08 18:54:44 +0200

    change travis file

--- tests/functional_tests/test_3_login.py
+++ tests/functional_tests/test_3_login.py
@@ -14,7 +14,7 @@ class LoginTest(unittest.TestCase):
 
     def test_login(self):
         # Check if Django is working
-        self.browser.get('localhost')
+        self.browser.get('http://localhost:8000')
         self.browser.maximize_window()
         self.assertIn('Pur Beurre', self.browser.title)
         time.sleep(3)
