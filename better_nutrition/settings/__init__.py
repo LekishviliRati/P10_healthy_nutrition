8b2b78860dd2bbc09f5e6f60d0bb0491f6ad43cc
LekishviliRati <lekishvili.rati@gmail.com>
2021-08-11 16:10:11 +0200

    changed senttings file database

--- better_nutrition/settings/__init__.py
+++ better_nutrition/settings/__init__.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'better_nutrition.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Use of postgresql
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Use of postgresql
         'NAME': 'better_nutrition',
         'USER': 'ratilekishvili',
         'PASSWORD': 'diTna7-kyztig*',
