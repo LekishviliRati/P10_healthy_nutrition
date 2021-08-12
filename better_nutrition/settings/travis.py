cd8760dbefedfdbdcccce51a7086b4e473cfaa0f
LekishviliRati <lekishvili.rati@gmail.com>
2021-08-12 11:22:46 +0200

    changed travis.py

--- better_nutrition/settings/travis.py
+++ better_nutrition/settings/travis.py
@@ -8,10 +8,10 @@ SECRET_KEY = "travis_secret_key"
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Use of postgresql
-        'NAME': 'better_nutrition',
+        'NAME': '',
         'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': '',
+        'PORT': '',
     },
 }
