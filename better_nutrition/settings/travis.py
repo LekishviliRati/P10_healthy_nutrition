a97cef702866d9db340ab6f53e0b8c2e23f452de
LekishviliRati <lekishvili.rati@gmail.com>
2021-08-14 19:19:09 +0200

    changed travis.yml file

--- better_nutrition/settings/travis.py
+++ better_nutrition/settings/travis.py
@@ -1,13 +1,24 @@
 from . import *
 
 
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.db.backends.postgresql',  # Use of postgresql
+#         'NAME': '',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     },
+# }
+
 DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.postgresql',  # Use of postgresql
-        'NAME': '',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Use of postgresql
+        'NAME': 'better_nutrition',
+        'USER': 'ratilekishvili',
+        'PASSWORD': 'diTna7-kyztig*',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
