663612790a8c773ceef142920d7e5f96950d1e49
LekishviliRati <lekishvili.rati@gmail.com>
2021-08-19 12:16:39 +0200

    Changed settings file, hide secret key

--- better_nutrition/settings/__init__.py
+++ better_nutrition/settings/__init__.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['165.22.66.94',
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Use of postgresql
         'NAME': 'better_nutrition',
         'USER': 'ratilekishvili',
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
